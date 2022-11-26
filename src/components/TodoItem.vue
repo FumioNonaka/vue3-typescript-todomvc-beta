f8f50715b97980676ed337281c36a9264f68491e
Fumio Nonaka <fumio@mx10.ttcn.ne.jp>
2022-11-26 20:02:07 +0900

    odoMVC 01 リスト項目の表示と追加

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Todo } from "../App.vue";
+
+interface Props {
+  todo: Todo;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="view">
+    <input type="checkbox" class="toggle" />
+    <label>{{ todo.title }}</label>
+  </div>
+</template>
