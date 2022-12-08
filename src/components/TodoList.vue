f24622bd290836fd5d0862186fa5825b4ce9c084
Fumio Nonaka <fumio@mx10.ttcn.ne.jp>
2022-12-08 17:13:29 +0900

    TodoMVC 02 リスト項目の削除とスタイル変更

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,14 +5,29 @@ import TodoItem from "./TodoItem.vue";
 interface Props {
   todos: Todo[];
 }
+interface Emits {
+  (event: "removeTodo", todo: Todo): void;
+  (event: "done", todo: Todo, completed: boolean): void;
+}
 defineProps<Props>();
+const emit = defineEmits<Emits>();
+const removeTodo = (todo: Todo) => {
+  emit("removeTodo", todo);
+};
+const done = (todo: Todo, completed: boolean) => {
+  emit("done", todo, completed);
+};
 </script>
 
 <template>
   <section class="main">
     <ul class="todo-list">
-      <li v-for="todo in todos" :key="todo.id">
-        <TodoItem :todo="todo" />
+      <li
+        v-for="todo in todos"
+        :class="['todo', { completed: todo.completed }]"
+        :key="todo.id"
+      >
+        <TodoItem :todo="todo" @removeTodo="removeTodo" @done="done" />
       </li>
     </ul>
   </section>
