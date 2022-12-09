bbba14bf21137181e1c0a502609ab60d00a61215
Fumio Nonaka <fumio@mx10.ttcn.ne.jp>
2022-12-09 20:07:38 +0900

    TodoMVC 03 算出プロパティとデータのローカルへの保存

---
+++ src/components/TodoController.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import type { Todo } from "../App.vue";
+
+interface Props {
+  todos: Todo[];
+  remaining: number;
+}
+const props = defineProps<Props>();
+const pluralize = computed(() => (props.remaining === 1 ? "item" : "items"));
+</script>
+
+<template>
+  <footer class="footer" v-show="todos.length">
+    <span class="todo-count">
+      <strong>{{ remaining }}</strong> {{ pluralize }} left
+    </span>
+  </footer>
+</template>
